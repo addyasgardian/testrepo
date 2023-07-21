3cbc4dfa13b269d8a71917dd246951c4087577a3
addyasgardian <132333288+addyasgardian@users.noreply.github.com>
2023-07-21 12:03:29 +0530

    commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use((req, res) => {
+  return res.sendFile(path.join(__dirname, "Portal", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`App is Ruuning on PORT ${PORT}`);
+});
